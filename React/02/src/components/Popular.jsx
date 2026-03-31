c90fced237db23688c6fd07fd40b7a766b455da8
Oleksii Kobzan <94406479+nemesic@users.noreply.github.com>
2026-04-01 01:04:31 +0300

    11

--- React/02/src/components/Popular.jsx
+++ React/02/src/components/Popular.jsx
@@ -1,10 +1,51 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import MovieCard from "./MovieCard";
 
-function Popular({ movies = [], search = "" , onSelectMovie}) {
+function Popular({ search = "" , onSelectMovie}) {
 const rowRef = useRef(null);
 
-const filteredMovies = movies.filter((movie) =>
+const [apiMovies, setApiMovies] = useState([]);
+const API_KEY = "9eee1c8a9ca4c5306eb86111905631a1";
+
+ useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [moviesRes, seriesRes] = await Promise.all([
+          fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+          ),
+          fetch(
+            `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+          ),
+        ]);
+
+        const moviesData = await moviesRes.json();
+        const seriesData = await seriesRes.json();
+
+        const moviesList = moviesData.results || [];
+        const seriesList = seriesData.results || [];
+
+        const combined = [...moviesList, ...seriesList];
+
+        const formatted = combined.map((item) => ({
+          id: item.id,
+          title: item.title || item.name,
+          img: item.poster_path
+            ? `https://image.tmdb.org/t/p/w500${item.poster_path}`
+            : "https://via.placeholder.com/300x450",
+          raw: item,
+        }));
+
+        setApiMovies(formatted);
+      } catch (error) {
+        console.log("TMDB error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+const filteredMovies = apiMovies.filter((movie) =>
   movie.title.toLowerCase().includes(search.toLowerCase()));
   
 const scrollLeft = () => {
@@ -34,7 +75,7 @@ return (
         </div>
         
         <div className="shows-row" ref={rowRef}>
-          {filteredMovies.length === 0 && <p>No movies found</p>}
+          {filteredMovies.length === 0 && <p>Loading...</p>}
 
           {filteredMovies.map((movie) => (
             <MovieCard 
